ad29462d9ba2e633b5a52d2375c7dd91d7d655cd
pajasson <1982konig@gmail.com>
2016-05-11 21:57:16 +0200

    Add files via upload

--- Environment.java
+++ Environment.java
@@ -3,20 +3,44 @@ import java.util.ArrayList;
 public class Environment {
 
     private int id=0;
+    private int width;
+    private int height;
     public int happening;
     private ArrayList<Event> regEvents;
-    private ArrayList<Node> nodes;
+    protected ArrayList<Node> nodes;
 
-    public Environment(int x, int y){
+    private void addNeighbour(Node n, Position p){
+        int x = p.getX();
+        int y = p.getY();
 
+        if(x > -1 && x < width && y > -1 && y < height){
+            n.neighbours.add(nodes.get(y * width + x));
+            //System.out.printf("\n %d %d ", x , y);
+        }
+    }
+
+    public Environment(int width, int height){
+        this.width = width;
+        this.height = height;
         int k=0;
         regEvents=new ArrayList();
         nodes = new ArrayList();
-        for(int i=0; i<y; i++) {
-            for(int j=0; j<x; i++) {
-                nodes.add(k, new Node(new Position(j,i)));
+        for(int y=0; y<height; y++) {
+            for(int x=0; x<width; x++) {
+                nodes.add(k, new Node(new Position(y,x)));
                 k++;
             }
+        }
+        for(Node n : nodes){
+            addNeighbour(n, n.p.getPosToEast());
+            addNeighbour(n, n.p.getPosToWest());
+            addNeighbour(n, n.p.getPosToSouth());
+            addNeighbour(n, n.p.getPosToNorth());
+            addNeighbour(n, n.p.getPosToSouthEast());
+            addNeighbour(n, n.p.getPosToSouthWest());
+            addNeighbour(n, n.p.getPosToNorthEast());
+            addNeighbour(n, n.p.getPosToNorthWest());
+
         }
     }
 
@@ -39,4 +63,3 @@ public class Environment {
 
     }
 }
-
