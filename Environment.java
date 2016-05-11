199aed261b90e236f1acc6a56bc35ad5fd86c1de
vijo0075 <viktorjonsson91@hotmail.com>
2016-05-11 16:22:47 +0200

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -4,10 +4,20 @@ public class Environment {
 
     private int id=0;
     public int happening;
-    private ArrayList<Event> regEvents = new ArrayList();
+    private ArrayList<Event> regEvents;
+    private ArrayList<Node> nodes;
 
     public Environment(int x, int y){
 
+        int k=0;
+        regEvents=new ArrayList();
+        nodes = new ArrayList();
+        for(int i=0; i<y; i++) {
+            for(int j=0; j<x; i++) {
+                nodes.add(k, new Node(new Position(j,i)));
+                k++;
+            }
+        }
     }
 
     public int createEventId(){
@@ -20,10 +30,13 @@ public class Environment {
     }
 
     public void addEvent(Event e){
+        regEvents.add(id,e);
 
     }
 
     public Node getNode(Position p){
+        return nodes.get(p.getX()+50*p.getY());
 
     }
 }
+
