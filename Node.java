a6f0a4be13d554b4560550591e3bd42cc4c61e31
vijo0075 <viktorjonsson91@hotmail.com>
2016-05-16 12:41:50 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Node {
 
-    private Position p;
-    private ArrayList<Node> neighbours = new ArrayList();
-    private ArrayList<Event> events = new ArrayList();
+    public Position p;
+    protected ArrayList<Node> neighbours = new ArrayList();
+   // private ArrayList<Event> events = new ArrayList();
     private ArrayList<Node> direction=new ArrayList();
     private ArrayList<Integer> distance=new ArrayList();
     private boolean visiting=false;
@@ -13,15 +14,21 @@ public class Node {
         this.p=p;
     }
 
-    public void sendAgent(){
-
+    public Agent sendAgent(int id, Node n){
+        Agent e = new Agent(id, n);
+        return e;
     }
 
     public void sendRequest(){
 
     }
 
-    public void setEvent(){
-
+    public boolean setEvent(int id){
+        Random rand = new Random();
+        if(rand.nextInt(10000)==5000){
+            distance.add(id, 0);
+            return true;
+        }
+        return false;
     }
 }
