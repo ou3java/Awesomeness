b675410826aac7348590c92e44d2b0de7c700db0
pajasson <1982konig@gmail.com>
2016-08-22 23:07:48 +0200

    Update Request.java

--- Request.java
+++ Request.java
@@ -19,22 +19,24 @@ public class Request extends Messenger {
     private Stack<Node> wayBack = new Stack();
     private Node startNode;
     private int event;
-    private boolean found= false;
+    private boolean found = false;
     private Node targetNode;
-    private boolean hasReturned=false;
+    private boolean hasReturned = false;
     private int stepsToGoal;
+    private int timeoutCounter;
 
     /**
      * Constructor for Request.
      * @param id The event-id to search for
      * @param n The starting node
      */
-    public Request(int id, Node n){
+    public Request(int id, Node n, int maxStep){
         event = id;
-        maxsteps=45;
         currentNode = n;
         startNode=n;
         currentNode.visiting = true;
+        this.maxsteps = maxStep;
+        timeoutCounter = 8 * maxsteps;
 
 
     }
@@ -58,54 +60,48 @@ public class Request extends Messenger {
      * If it has found its event it writes the information to the console
      */
     private void dumpInfo(){
-        if(targetNode==null && hasReturned==false){
-            hasReturned=true;
-            maxsteps=45;
-            wayBack.push(startNode);
-        }
-        else{
-            String reqStart= "x:"+(startNode.p.getX()+1)+" y:"+(startNode.p.getY()+1);
-            String reqEnd = "x:"+(targetNode.p.getX()+1)+" y:"+(targetNode.p.getY()+1);
-            System.out.println("Event "+event+" found!");
-            System.out.println("Request made by node: "+reqStart+"");
-            System.out.println("Event was found in node: "+reqEnd+"");
-            System.out.println("Event time: "+targetNode.routTable.get(event).time+"");
-            System.out.println("Steps it took: "+stepsToGoal+"\n");
 
-            maxsteps = 0;
-        }
+        String reqStart= "x:"+(startNode.p.getX()+1)+" y:"+(startNode.p.getY()+1);
+        String reqEnd = "x:"+(targetNode.p.getX()+1)+" y:"+(targetNode.p.getY()+1);
+        System.out.println("Event "+event+" found!");
+        System.out.println("Request made by node: "+reqStart+"");
+        System.out.println("Event was found in node: "+reqEnd+"");
+        System.out.println("Event time: "+targetNode.routTable.get(event).time+"");
+        System.out.println("Steps it took: "+stepsToGoal+"\n");
+
+        maxsteps = 0;
+
     }
 
     /**
      * This method simulates a move the Request makes from one node to another.
      */
     public void move(){
+        timeoutCounter--;
+        if(timeoutCounter <= 0 && !hasReturned){
+            hasReturned=true;
+            maxsteps=45;
+            wayBack.clear();
+            wayBack.push(startNode);
+        }
         //if the request has returned after either found the event or ran out
         //of steps...
-        Boolean returning = found || maxsteps <=0;
-        if(wayBack.isEmpty() && returning){
-            //Do the dumpinfomethod and ignore the rest of move
-            dumpInfo();
-            return;
-        }
-        //Or if it has found its event or has ran out of steps...
-        else if(found||maxsteps==0){
-            //...queue the last node to nodesToVisit.
-            nodesToVisit=wayBack.peek();
-
-        }
-        //Or if it has found no information about its event in its node...
-        else if (currentNode.routTable.get(event)==null){
-            //...choose a random node among its neighbours
-            Node n = currentNode.randomNeighbour();
-            if(wayBack.isEmpty()){
-                n = currentNode.randomNeighbour();
+        if(found || maxsteps <= 0) {
+            if (wayBack.isEmpty()) {
+                //Do the dumpinfomethod and ignore the rest of move
+                dumpInfo();
+                return;
             }
             else{
-                while (n == wayBack.peek() || n == null) {
-                    n = currentNode.randomNeighbour();
-                }
+                nodesToVisit = wayBack.peek();
             }
+        }
+
+        //Or if it has found no information about its event in its node...
+        else if (currentNode.routTable.get(event)==null){
+
+            //Continue with random neighbour
+            Node n = currentNode.randomNeighbour();
             nodesToVisit = n;
         }
         //Or if informaion about its event is found...
@@ -113,12 +109,13 @@ public class Request extends Messenger {
             //...choose the correct node from the table and add to nodesToVisit.
             nodesToVisit = currentNode.routTable.get(event).node;
         }
+
         //If the node in nodesToVisit is not visided already...
         if(!nodesToVisit.visiting){
             //make the move by changing the nodevalues.
             currentNode.visiting = false;
             //different behavior if returning.
-            if(returning) {
+            if(found) {
                 wayBack.pop();
             }
             else {
