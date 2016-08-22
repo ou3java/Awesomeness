55870908fe23a05fa27ea71a956f7602dde1ba76
pajasson <1982konig@gmail.com>
2016-08-22 23:05:40 +0200

    Update Messenger.java

--- Messenger.java
+++ Messenger.java
@@ -6,6 +6,7 @@
  * id15jnn
  * dv15nkn
  */
+import java.util.LinkedList;
 
 /**
  * This is a parentclass for agent and request.
@@ -15,7 +16,8 @@ public class Messenger {
     protected int maxsteps;
     protected Node nodesToVisit=null;
     protected Node currentNode;
-
+    protected LinkedList<Node> visitedNodes = new LinkedList<>();
+    //protected Set<Node> visitedNodes = new Set<Node>();
     public Messenger(){
     }
 
