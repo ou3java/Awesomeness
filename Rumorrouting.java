51c7167a13743f80a5cbba5439a99656206eba14
pajasson <1982konig@gmail.com>
2016-08-22 23:10:12 +0200

    Update Rumorrouting.java

--- Rumorrouting.java
+++ Rumorrouting.java
@@ -12,9 +12,21 @@
  */
 public class Rumorrouting {
 
+    // Program parameters
+
+    //chance a node spawns an event. 1.0 = 100%,  0.0001 = 0.01%
+    static final double eventChance = 0.0001;
+
+    //maximum steps for agent and request
+    static final int stepsAgent = 50;
+    static final int stepsReq = 45;
+
+
+
+
     public static void main(String [] args){
         //Create a nodegrid with 50*50 nodes
-        Environment env = new Environment(50, 50);
+        Environment env = new Environment(50, 50, stepsAgent, stepsReq, eventChance);
         //run the nodeenvironment 10000 ticks
         for(int i = 0;i < 10000;i++){
            env.timeTick();
@@ -22,3 +34,4 @@ public class Rumorrouting {
 
     }
 }
+
